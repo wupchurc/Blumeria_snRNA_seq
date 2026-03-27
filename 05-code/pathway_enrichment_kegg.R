14385435b76ae1e3545973632ea85de8783ea068
Weston <wupchurc@umn.edu>
2026-03-27 13:36:34 -0500

    use gsea for pathway analysis with KEGG

--- 05-code/pathway_enrichment_kegg.R
+++ 05-code/pathway_enrichment_kegg.R
@@ -5,81 +5,113 @@ library(DESeq2)
 library(ggtangle)
 library(ggplot2)
 
-cm_results <- readRDS("rds_files/DEG_Cardiomyocytes.rds")
+cm_results <- readRDS("03-analysis_scratch/DEG_Cardiomyocytes.rds")
+fb_results <- readRDS("03-analysis_scratch/DEG_Fibroblasts.rds")
+mac_results <- readRDS("03-analysis_scratch/DEG_Macrophages.rds")
 
-# Function to process one DESeqResults → up/down ENTREZ lists
-get_sig_genes <- function(res, lfc_threshold = 0, padj_threshold = 0.1) {
-  df <- as.data.frame(res)
-  df$gene <- rownames(res)
+analyze_celltype_gsea <- function(results_obj, cell_name) {
   
-  id_map <- bitr(df$gene, fromType="SYMBOL", toType="ENTREZID", OrgDb=org.Rn.eg.db)
-  df <- merge(df, id_map, by.x="gene", by.y="SYMBOL")
+  # Function to get ranked list
+  get_ranked_list <- function(res) {
+    df <- as.data.frame(res)
+    df$gene <- rownames(res)
+    id_map <- bitr(df$gene, fromType="SYMBOL", 
+                   toType="ENTREZID", OrgDb=org.Rn.eg.db)
+    df <- merge(df, id_map, by.x="gene", by.y="SYMBOL")
+    df <- df[!is.na(df$ENTREZID) & !is.na(df$log2FoldChange), ]
+    gene_list <- df$log2FoldChange
+    names(gene_list) <- df$ENTREZID
+    gene_list <- sort(gene_list, decreasing = TRUE)
+    return(gene_list)
+  }
   
-  up   <- df$ENTREZID[df$padj < padj_threshold & df$log2FoldChange >  lfc_threshold]
-  down <- df$ENTREZID[df$padj < padj_threshold & df$log2FoldChange < -lfc_threshold]
+  ranked_lists <- list(
+    water_vs_ctrl = get_ranked_list(results_obj$water_vs_ctrl),
+    water_vs_blum = get_ranked_list(results_obj$water_vs_blum)
+  )
   
-  list(up = up, down = down, all = df$ENTREZID)
+  gsea_df <- do.call(rbind, lapply(names(ranked_lists), function(nm) {
+    data.frame(
+      ENTREZID = names(ranked_lists[[nm]]),
+      log2FoldChange = ranked_lists[[nm]],
+      contrast_name = nm,
+      stringsAsFactors = FALSE
+    )
+  }))
+  # Set desired order here
+  gsea_df$contrast_name <- factor(gsea_df$contrast_name,
+                                  levels = c("water_vs_ctrl", "water_vs_blum"))
+  
+  comp_gsea <- compareCluster(
+    ENTREZID | log2FoldChange ~ contrast_name,
+    data = gsea_df,
+    fun = "gseKEGG",
+    organism = "rno",           # rat KEGG code
+    keyType = "ncbi-geneid",    # KEGG expects this for NCBI IDs
+    pvalueCutoff = 0.05,
+    pAdjustMethod = "BH",
+    minGSSize = 10,
+    maxGSSize = 500,
+    verbose = FALSE
+  )
+  
+  # Make sure Cluster is ordered consistently for plotting
+  comp_gsea@compareClusterResult$Cluster <- factor(
+    comp_gsea@compareClusterResult$Cluster,
+    levels = c("water_vs_ctrl", "water_vs_blum")
+  )
+  
+  list(gsea = comp_gsea, ranked_lists = ranked_lists)
 }
 
-# Extract sig lists for ALL contrasts
-sig_lists <- list(
-  water_vs_ctrl = get_sig_genes(cm_results$water_vs_ctrl),
-  blum_vs_ctrl  = get_sig_genes(cm_results$blum_vs_ctrl),
-  blum_vs_water = get_sig_genes(cm_results$blum_vs_water),
-  water_vs_blum = get_sig_genes(cm_results$water_vs_blum)
+
+#----
+gsea_res <- analyze_celltype_gsea(cm_results, "Cardiomyocytes")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/CM_GSEA_KEGG_results.csv",
+  row.names = FALSE
 )
 
-# Individual ORA results (up/down per contrast)
-ego_up_all <- list(
-  water_vs_ctrl = enrichKEGG(
-    gene      = sig_lists$water_vs_ctrl$up,
-    universe  = sig_lists$water_vs_ctrl$all,
-    organism  = "rno",            # rat
-    keyType   = "ncbi-geneid",           # or "ncbi-geneid"/"entrezid" depending on IDs
-    pvalueCutoff = 0.1,
-    qvalueCutoff = 0.1
-  ),
-  water_vs_blum = enrichKEGG(
-    gene      = sig_lists$water_vs_blum$up,
-    universe  = sig_lists$water_vs_blum$all,
-    organism  = "rno",
-    keyType   = "ncbi-geneid",
-    pvalueCutoff = 0.1,
-    qvalueCutoff = 0.1
-  )
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("CM GSEA - Top Terms") +
+  theme(axis.text.y = element_text(size = 9))
+
+#
+gsea_res <- analyze_celltype_gsea(mac_results, "Macrophages")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/CM_GSEA_KEGG_results.csv",
+  row.names = FALSE
 )
 
-ego_down_all <- list(
-  water_vs_ctrl = enrichKEGG(
-    gene      = sig_lists$water_vs_ctrl$down,
-    universe  = sig_lists$water_vs_ctrl$all,
-    organism  = "rno",            # rat
-    keyType   = "ncbi-geneid",           # or "ncbi-geneid"/"entrezid" depending on IDs
-    pvalueCutoff = 0.1,
-    qvalueCutoff = 0.1
-  ),
-  water_vs_blum = enrichKEGG(
-    gene      = sig_lists$water_vs_blum$down,
-    universe  = sig_lists$water_vs_blum$all,
-    organism  = "rno",
-    keyType   = "ncbi-geneid",
-    pvalueCutoff = 0.1,
-    qvalueCutoff = 0.1
-  )
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Macrophages - Top Terms") +
+  theme(axis.text.y = element_text(size = 9))
+
+# 
+gsea_res <- analyze_celltype_gsea(fb_results, "Fibroblasts")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/CM_GSEA_KEGG_results.csv",
+  row.names = FALSE
 )
 
-
-dotplot(ego_up_all$water_vs_blum, showCategory=20, font.size = 6,title = "Water vs Blum - Upregulated")
-dotplot(ego_down_all$water_vs_blum, showCategory=20, font.size = 6,title = "Water vs Blum - ")
-
-
-# Download rno04216 genes (rat ferroptosis)
-library(KEGGREST)
-rno04216_genes <- keggGet("rno04216")[[1]]$GENE  # returns "entrez:gene_name"
-rno_genes <- sub("rno:([0-9]+).*", "\\1", rno04216_genes)  # extract Entrez IDs
-
-# Test your sig genes against this pathway
-ferro_test <- enricher(gene = sig_lists$water_vs_ctrl$up,
-                       TERM2GENE = data.frame(ID="rno04216", Gene=rno_genes),
-                       pvalueCutoff=0.1)
-ferro_test
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Fibroblasts - Top Terms") +
+  theme(axis.text.y = element_text(size = 9))
