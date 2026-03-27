8340b198aed2b33679c3ae6e78199e788afd39b6
Weston <wupchurc@umn.edu>
2026-03-27 15:57:23 -0500

    add every cell type

--- 05-code/pathway_enrichment_kegg.R
+++ 05-code/pathway_enrichment_kegg.R
@@ -8,6 +8,14 @@ library(ggplot2)
 cm_results <- readRDS("03-analysis_scratch/DEG_Cardiomyocytes.rds")
 fb_results <- readRDS("03-analysis_scratch/DEG_Fibroblasts.rds")
 mac_results <- readRDS("03-analysis_scratch/DEG_Macrophages.rds")
+nphl_results <- readRDS("03-analysis_scratch/DEG_Neutrophils.rds")
+peri_results <- readRDS("03-analysis_scratch/DEG_Pericytes.rds")
+bcells_results <- readRDS("03-analysis_scratch/DEG_B_cells.rds")
+cap_results <- readRDS("03-analysis_scratch/DEG_Capillary_EC.rds")
+lymph_results <- readRDS("03-analysis_scratch/DEG_Lymphatic_EC.rds")
+neuro_results <- readRDS("03-analysis_scratch/DEG_Neuronal.rds")
+tcell_results <- readRDS("03-analysis_scratch/DEG_T_cells.rds")
+ven_results <- readRDS("03-analysis_scratch/DEG_Venous_EC.rds")
 
 analyze_celltype_gsea <- function(results_obj, cell_name) {
   
@@ -65,7 +73,7 @@ analyze_celltype_gsea <- function(results_obj, cell_name) {
 }
 
 
-#----
+#---- Cardiomyocytes ----
 gsea_res <- analyze_celltype_gsea(cm_results, "Cardiomyocytes")
 
 gsea_df <- as.data.frame(gsea_res$gsea)
@@ -79,10 +87,10 @@ write.csv(
 
 dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
   facet_grid(. ~ .sign) +
-  ggtitle("CM GSEA - Top Terms") +
+  ggtitle("Cardiomyocytes - KEGG") +
   theme(axis.text.y = element_text(size = 9))
 
-#
+#----
 gsea_res <- analyze_celltype_gsea(mac_results, "Macrophages")
 
 gsea_df <- as.data.frame(gsea_res$gsea)
@@ -90,13 +98,13 @@ head(gsea_df)
 
 write.csv(
   gsea_df,
-  file = "04-results/CM_GSEA_KEGG_results.csv",
+  file = "04-results/MAC_GSEA_KEGG_results.csv",
   row.names = FALSE
 )
 
 dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
   facet_grid(. ~ .sign) +
-  ggtitle("Macrophages - Top Terms") +
+  ggtitle("Macrophages - KEGG") +
   theme(axis.text.y = element_text(size = 9))
 
 # 
@@ -107,11 +115,146 @@ head(gsea_df)
 
 write.csv(
   gsea_df,
-  file = "04-results/CM_GSEA_KEGG_results.csv",
+  file = "04-results/FB_GSEA_KEGG_results.csv",
   row.names = FALSE
 )
 
 dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
   facet_grid(. ~ .sign) +
-  ggtitle("Fibroblasts - Top Terms") +
+  ggtitle("Fibroblasts - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+
+
+#---- Neutrophils ----
+gsea_res <- analyze_celltype_gsea(nphl_results, "Neutrophils")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/NPHL_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Neutrophils - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+
+#---- B cells ----
+gsea_res <- analyze_celltype_gsea(bcells_results, "B cells")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/BCELLS_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("B cells - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+#---- Pericytes ----
+gsea_res <- analyze_celltype_gsea(peri_results, "Pericytes")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/PERI_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Pericytes - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+
+# ---- Capillary EC ----
+gsea_res <- analyze_celltype_gsea(cap_results, "Capillary EC")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/CAPILLARYEC_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Capillary EC - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+
+
+#---- Lymphatic EC ----
+gsea_res <- analyze_celltype_gsea(lymph_results, "Lymphatic EC")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/LYMPHATICEC_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Lymphatic EC - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+# ---- Neuronal ----
+gsea_res <- analyze_celltype_gsea(neuro_results, "Neuronal")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/NEURONAL_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Neuronal - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+# ---- T cells ----
+gsea_res <- analyze_celltype_gsea(tcell_results, "T cells")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/TCELL_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("T cells - KEGG") +
+  theme(axis.text.y = element_text(size = 9))
+
+# ---- Venous EC ----
+gsea_res <- analyze_celltype_gsea(ven_results, "Venous EC")
+
+gsea_df <- as.data.frame(gsea_res$gsea)
+head(gsea_df)
+
+write.csv(
+  gsea_df,
+  file = "04-results/VENOUSEC_GSEA_KEGG_results.csv",
+  row.names = FALSE
+)
+
+dotplot(gsea_res$gsea, showCategory = 10, split = ".sign") +
+  facet_grid(. ~ .sign) +
+  ggtitle("Venous EC - KEGG") +
   theme(axis.text.y = element_text(size = 9))
